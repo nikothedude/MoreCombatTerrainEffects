a0c93c7390fe68e6e89563f34f9b76d2cf5fa655
nikothedude <59709059+nikothedude@users.noreply.github.com>
2022-10-31 12:36:18 -0400

    Update MCPE_settings.kt

--- jars/src/niko/MCTE/utils/MCPE_settings.kt
+++ jars/src/niko/MCTE/utils/MCPE_settings.kt
@@ -7,6 +7,12 @@ import kotlin.jvm.Throws
 
 object MCPE_settings {
     var SHOW_ERRORS_IN_GAME: Boolean = true
+    var MAG_FIELD_EFFECT_ENABLED: Boolean = true
+    var DEEP_HYPERSPACE_EFFECT_ENABLED: Boolean = true
+    var SLIPSTREAM_EFFECT_ENABLED: Boolean = true
+    var DEBRIS_FIELD_EFFECT_ENABLED: Boolean = true
+    var DUST_CLOUD_EFFECT_ENABLED: Boolean = true
+    var EXTRA_NEBULA_EFFECTS_ENABLED: Boolean = true
 
     @JvmStatic
     @Throws(JSONException::class, IOException::class)
@@ -14,5 +20,11 @@ object MCPE_settings {
         MCPE_debugUtils.log.info("reloading settings")
         val configJson = Global.getSettings().loadJSON(MCPE_ids.masterConfig)
         SHOW_ERRORS_IN_GAME = configJson.getBoolean("showErrorsInGame")
+        MAG_FIELD_EFFECT_ENABLED: Boolean = configJson.getBoolean("enableMagFieldEffect")
+        DEEP_HYPERSPACE_EFFECT_ENABLED: Boolean = configJson.getBoolean("enableDeepHyperspaceEffect")
+        SLIPSTREAM_EFFECT_ENABLED: Boolean = configJson.getBoolean("enableSlipstreamEffect")
+        DEBRIS_FIELD_EFFECT_ENABLED: Boolean = configJson.getBoolean("enableDebrisFieldEffect")
+        DUST_CLOUD_EFFECT_ENABLED: Boolean = configJson.getBoolean("enableDustcloudEffect")
+        EXTRA_NEBULA_EFFECTS_ENABLED: Boolean = configJson.getBoolean("enableExtraNebulaEffects")
     }
 }
